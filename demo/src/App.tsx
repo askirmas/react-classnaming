ddfd3c3f246e3494801a67445ed8ef4c47635905
Andrii Kirmas <kirmas@phystech.edu>
2021-02-06 21:09:40 +0300

    Change CRA example to have css module

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,14 +1,16 @@
-import './App.css';
+import AppClassNames from './App.module.css';
+
+const {App__container, App__header, App__link} = AppClassNames
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={App__container}>
+      <header className={App__header}>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={App__link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
