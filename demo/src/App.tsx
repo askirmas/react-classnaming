547413417a5e1ea309ca01e00abf746e4c62c7e1
Andrii Kirmas <kirmas@phystech.edu>
2021-02-06 21:17:00 +0300

    Change CRA demo to use compiled classNaming

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,20 +1,18 @@
+import classNaming from "react-classnaming"
 import AppClassNames from './App.module.css';
 
 const {App__container, App__header, App__link} = AppClassNames
 
 function App() {
   return (
-    <div className={App__container}>
-      <header className={App__header}>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={App__link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <div {...classNaming({App__container})}>
+      <header className={classNaming<string>({App__header})}>
+        <a {...{
+          ...classNaming({App__link}),
+          "href": "https://reactjs.org",
+          "target": "_blank",
+          "rel": "noopener noreferrer",
+        }}>
           Learn React
         </a>
       </header>
