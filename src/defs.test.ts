1a25788ed22fb95c816ed801958b76000e6417fb
Andrii Kirmas <kirmas@phystech.edu>
2021-02-20 18:06:58 +0300

    Add props extending

--- src/defs.test.ts
+++ src/defs.test.ts
@@ -1,7 +1,10 @@
+import type { JSXElementConstructor } from "react";
 import { Component } from "react";
-import type { ClassNamesStrict, GetClassKeys } from "./defs";
+import type { ClassNamesProp, ClassNamesStrict, GetClassKeys } from "./defs";
 
-class ClassComponent extends Component<ClassNamesStrict<never, "comp1"|"comp2">> {}
+class ClassComponent0 extends Component<ClassNamesStrict<never, "comp1"|"comp2">> {}
+class ClassComponent1 extends Component<ClassNamesStrict<true, never>> {}
+class ClassComponent2 extends Component<ClassNamesStrict<true, "comp1"|"comp2">> {}
 
 describe("ClassNamesStrict", () => {
   it("<never, 'class1'|'class2'>", () => {
@@ -15,8 +18,8 @@ describe("ClassNamesStrict", () => {
     }
     expect(suites).toBeInstanceOf(Object)
   })
-  it("", () => {
-    const suites: Record<string, ClassNamesStrict<never, never, typeof ClassComponent>> = {
+  it("ClassComponent0", () => {
+    const suites: Record<string, ClassNamesStrict<never, never, typeof ClassComponent0>> = {
       "good": {
         classNames: {
           comp1: undefined,
@@ -38,7 +41,7 @@ describe("ClassNamesStrict", () => {
 
 describe("GetClassKeys", () => {
   it("ClassComponent", () => {
-    const suites: Record<string, GetClassKeys<typeof ClassComponent>> = {
+    const suites: Record<string, GetClassKeys<typeof ClassComponent0>> = {
       "comp1": "comp1",
       "comp2": "comp2",
       //@ts-expect-error
@@ -46,4 +49,21 @@ describe("GetClassKeys", () => {
     }
     expect(suites).toBeInstanceOf(Object)
   })
+})
+
+it("WithClassNames", () => {
+  type WithClassNames = ClassNamesProp | JSXElementConstructor<ClassNamesProp>
+  // | Component<ClassNamesProp> | (props: ClassNamesProp) => any
+
+  const suites: Record<string, WithClassNames> = {
+    ClassComponent0,
+    //TODO
+    //@ts-expect-error Property 'className' is missing
+    ClassComponent1,
+    //TODO
+    //@ts-expect-error Property 'className' is missing
+    ClassComponent2,
+  }
+
+  expect(suites).toBeInstanceOf(Object)
 })
--- src/defs.ts
+++ src/defs.ts
@@ -14,7 +14,17 @@ export type ClassNames<
 : C0 extends string ? ClassNamesStrict<never, C0>
 : never
 
-export type ClassNamesStrict<C0 extends true | never, C1 extends string | never, C2=never, C3=never, C4=never, C5=never, C6=never, C7=never, C8=never, C9=never, C10=never, C11=never>
+export type ClassNamesStrict<C0 extends true | never, C1 extends string | never = never,
+C2=never,
+C3=never,
+C4=never,
+C5=never,
+C6=never,
+C7=never,
+C8=never,
+C9=never,
+C10=never,
+C11=never>
 = Ever<C0, {className: string}> & ClassNamesProp<C1
 | GetClassKeys<C2>
 | GetClassKeys<C3>
@@ -28,10 +38,22 @@ export type ClassNamesStrict<C0 extends true | never, C1 extends string | never,
 | GetClassKeys<C11>
 >
 
-export type ClassNamesFrom<C1, C2=never, C3=never, C4=never, C5=never, C6=never, C7=never, C8=never, C9=never, C10=never>
-= ClassNamesStrict<never, never, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10>
+export type ClassNamesFrom<
+C1 extends ReactRelated,
+C2 extends ReactRelated = never,
+C3 extends ReactRelated = never,
+C4 extends ReactRelated = never,
+C5 extends ReactRelated = never,
+C6 extends ReactRelated = never,
+C7 extends ReactRelated = never,
+C8 extends ReactRelated = never,
+C9 extends ReactRelated = never,
+C10 extends ReactRelated = never
+> = ClassNamesStrict<never, never, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10>
 
-export type ClassNamesProp<C extends string> = Ever<C, {classNames: ClassNamesMap<C>}>
+export type ReactRelated = Record<string, any> | JSXElementConstructor<any>
+
+export type ClassNamesProp<C extends string = string> = Ever<C, {classNames: ClassNamesMap<C>}>
 
 export type ClassNamesMap<C extends string> = Record<C, undefined|string>
 
