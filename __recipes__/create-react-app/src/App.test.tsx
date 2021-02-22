c5e8bf9cb37aa94eca2230e27dcecd6460d5262e
Andrii Kirmas <kirmas@phystech.edu>
2021-02-22 14:14:16 +0300

    Change CRA testing to mocking

--- __recipes__/create-react-app/src/App.test.tsx
+++ __recipes__/create-react-app/src/App.test.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('snapshot', () => {
+  expect(renderer.create(<App />).toJSON()).toMatchSnapshot()
 });
