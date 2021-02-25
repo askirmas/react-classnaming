8161fe7971e90687952c683241967454b9ea1904
Andrii Kirmas <kirmas@phystech.edu>
2021-02-25 22:04:12 +0300

    Test issue of exclusion from `tring`

--- src/sandbox/omitters/pipe-with-left.ts
+++ src/sandbox/omitters/pipe-with-left.ts
@@ -2,35 +2,23 @@ import { ClassValue } from "../../defs"
 
 export {}
 
-// Nope
-// type tExcluder<S extends Record<string, ClassValue>>
-// = (
-//   <E extends{[K in keyof S]?: ClassValue}>(exclude: E) => typeof exclude extends Record<infer E, ClassValue>
-//   ? { [P in Exclude<keyof S, keyof E>]: ClassValue; } & tExcluder<{ [P in Exclude<keyof S, keyof E>]: ClassValue; }>
-//   : never
-// );
-
-
-// type tExcluder<S extends Record<string, ClassValue>>
-// = (
-//   <E extends Record<string, ClassValue>>(exclude: E) => { [P in Exclude<keyof S, keyof E>]: ClassValue; }
-//   & tExcluder<{ [P in Exclude<keyof S, keyof E>]: ClassValue; }>
-// );
-
-// type tExcluder<S extends Record<string, ClassValue>>
-// = (
-//   <E extends Record<string, ClassValue>>(exclude: E) => keyof E extends keyof S
-//   ? { [P in Exclude<keyof S, keyof E>]: ClassValue; }
-//     & tExcluder<{ [P in Exclude<keyof S, keyof E>]: ClassValue; }>
-//   : {[P in Exclude<keyof E, keyof S>]: never;}
-// );
-
-type tExcluder<S extends Record<string, ClassValue>>
+type tExcluder<
+  Source extends Record<string, ClassValue>,
+  Left extends Record<string, ClassValue> = Source,
+  Used extends Record<string, ClassValue> = Record<never, ClassValue>,
+>
 = (
-  <E extends {[K in keyof S]?: ClassValue}>(exclude: E) =>
+  <
+    // E extends 
+    E extends {[K in keyof Left]?: K extends keyof Used ? never : ClassValue}
+  >(exclude: E) =>
   //  keyof E extends keyof S ?
-   { [K in Exclude<keyof S, keyof E>]: ClassValue; }
-    & tExcluder<{ [P in Exclude<keyof S, keyof E>]: ClassValue; }>
+   { [K in Exclude<keyof Left, keyof E>]: ClassValue; }
+    & tExcluder<
+      Source,
+      { [P in Exclude<keyof Left, keyof E>]: P extends keyof Used ? never : ClassValue; },
+      { [P in keyof Used | keyof E]: ClassValue; }
+    >
   // : {[P in Exclude<keyof E, keyof S>]: never;}
 );
 
@@ -59,7 +47,11 @@ function exclusion<
     host[key]
     = filtered[key]
 
-  return host as tExcluder<{ [P in Exclude<keyof S, keyof E>]: ClassValue; }>
+  return host as tExcluder<
+    S,
+    {[P in Exclude<keyof S, keyof E>]: ClassValue;},
+    E
+  >
 }
 
 const source: Record<"a"|"b"|"c"|"d"|"e", ClassValue> = {a: "a", b: undefined, c: "c", d: undefined, e: undefined}
@@ -109,3 +101,15 @@ const unknown0 = exclusion({} as Record<string, ClassValue>, {a: "a"})
 , unknown1 = unknown0({a: "a"})
 
 export {answ0, step3, checks, unknown1}
+
+// type Additinioze<A, T0, E = never> = {
+//   [P in keyof A]: A[P] extends E ? T0 : A[P] 
+// }
+// type Additional<K extends string, T1, T2, E = never> = Additinioze<AntiRecord<K, T2, true, E>, T1, E>
+// type AntiRecord<R extends string, T, Strict extends boolean, E = never>
+// = {[k: string]: T}
+// & (
+//   Strict extends true
+//   ? {[k in R]: E }
+//   : {[k in R]?: E }
+// )
