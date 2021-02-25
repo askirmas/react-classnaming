a15e71bd67a044a07fd5d78ac6be02c467007b4c
Andrii Kirmas <kirmas@phystech.edu>
2021-02-25 23:08:28 +0300

    #1 Remove `GetClassKeys` from *defs.test*

--- src/defs.test.ts
+++ src/defs.test.ts
@@ -1,21 +1,8 @@
-import { Component } from "react";
-import type { ClassNamesProp, GetClassKeys, GetClassNames } from "./defs";
+import type { GetClassNames } from "./defs";
 
-class ClassComponent0 extends Component<ClassNamesProp<"comp1"|"comp2">> {}
-
-describe("GetClassKeys", () => {
-  it("ClassComponent", () => {
-    const suites: Record<string, GetClassKeys<typeof ClassComponent0>> = {
-      "comp1": "comp1",
-      "comp2": "comp2",
-      //@ts-expect-error
-      "bad": "xxx" 
-    }
-    expect(suites).toBeInstanceOf(Object)
-  })
-
-  it("no classNames", () => {
-      const suites: Record<string, GetClassKeys<{}>> = {
+describe("GetClassNames", () => {
+  it("props without classnames", () => {
+    const suites: Record<string, GetClassNames<{}>> = {
       //@ts-expect-error is not assignable to type 'never'
       "undefined": undefined,
       //@ts-expect-error is not assignable to type 'never'
@@ -30,27 +17,5 @@ describe("GetClassKeys", () => {
       "{}": {}
     }
     expect(suites).toBeInstanceOf(Object)
-  })  
+  })
 })
-
-describe("GetClassNames", () => {
-  it("props without classnames", () => {
-
-  const suites: Record<string, GetClassNames<{}>> = {
-    //@ts-expect-error is not assignable to type 'never'
-    "undefined": undefined,
-    //@ts-expect-error is not assignable to type 'never'
-    "false": false,
-    //@ts-expect-error is not assignable to type 'never'
-    "null": null,
-    //@ts-expect-error is not assignable to type 'never'
-    "": "",
-    //@ts-expect-error is not assignable to type 'never'
-    "0": 0,
-    //@ts-expect-error is not assignable to type 'never'
-    "{}": {}
-  }
-  expect(suites).toBeInstanceOf(Object)
-})
-})
-
