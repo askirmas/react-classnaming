1da5a139db2b30b0cf8cff63c8bb818b692df948
Andrii Kirmas <kirmas@phystech.edu>
2021-03-02 03:31:22 +0300

    #20 Add context binder of ` classNamesMap`

--- src/map.ts
+++ src/map.ts
@@ -1,4 +1,8 @@
-import { ClassHash, ClassNamesProperty, CssModule } from "./defs";
+import {
+  Action,
+  ClassNamesProperty,
+  CssModule
+} from "./defs";
 import {resolver} from "./core"
 
 const {keys: $keys} = Object
@@ -6,11 +10,28 @@ const {keys: $keys} = Object
 export default classNamesMap
 
 function classNamesMap<
+  Source extends CssModule,
+>(classnames: Source){
+  const mapper: ClassNamesMap<Source> = map => mapping(classnames, map)
+  return mapper
+}
+
+type ClassNamesMap<Source extends CssModule> = (
+  <
+    Target extends ClassNamesProperty<TargetClasses>,
+    TargetClasses extends CssModule = CssModule
+  >(map: {
+    [T in keyof Target["classnames"]]:
+      {[S in keyof Source]?: Action}
+  }) => {classnames: {[T in keyof Target["classnames"]]: string}}
+)
+
+function mapping<
   Target extends ClassNamesProperty<CssModule>,
-  Source extends ClassNamesProperty<CssModule>,
->(map: {
+  Source extends CssModule,
+>(source: Source, map: {
   [T in keyof Target["classnames"]]:
-    {[S in keyof Source["classnames"]]?: ClassHash}
+    {[S in keyof Source]?: Action}
   }
 ): {classnames: {[T in keyof Target["classnames"]]: string}} {
   const keys = $keys(map) as (keyof Target["classnames"])[]
@@ -19,8 +40,9 @@ function classNamesMap<
   for (let i = keys.length; i--;) {
     const key = keys[i]
     
-    classnames[key] = resolver(undefined, map[key]).join(" ")
+    classnames[key] = resolver(source, map[key]).join(" ")
   }
 
   return {classnames}
 }
+
