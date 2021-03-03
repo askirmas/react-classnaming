7989a68180c4405cae1593fe3590398c6324e358
Andrii Kirmas <kirmas@phystech.edu>
2021-03-04 02:30:43 +0300

    Move react typings test to separate module

--- src/defs.test.ts
+++ src/defs.test.ts
@@ -3,15 +3,10 @@ import {
   PureComponent
 } from "react"
 import type {
-  ClassNamed,
   ClassHash,
-  ReactRelated,
   ClassNamesProperty,
   ClassNames
 } from "./defs"
-import {
-  GetProps
-} from "./react-swiss"
 
 type Props = ClassNames<true, ClassNamesProperty<{props: ClassHash}>>
 function Functional(_: ClassNames<ClassNamesProperty<{functional: ClassHash}>>) { return null }
@@ -259,63 +254,4 @@ describe("ClassNamesProperty", () => {
     }
     expect({suite4wrong, suite}).toBeInstanceOf(Object)
   })
-})
-
-
-describe("ReactRelated", () => {
-  type Getter<T extends ReactRelated> = GetProps<T>
-
-  type Without = ClassNamed
-  type Wrong = ClassNamed & {classnames: string}
-  type Some = ClassNamed & {classnames: Record<string, ClassHash>}
-  type Props = ClassNamed & {classnames: {class1: ClassHash; class2: ClassHash;}}
-
-  it("Component", () => {
-    class RWithout extends Component<Without> {};
-    class RWrong extends Component<Wrong> {};
-    class RSome extends Component<Some> {};
-    class RProps extends Component<Props> {};
-
-    //@ts-expect-error
-    type x =
-    | Getter<typeof RProps>
-    //@ts-expect-error
-    type Res =
-    //@ts-expect-error
-    | Getter<typeof RWithout>
-    //@ts-expect-error
-    | Getter<typeof RWrong> 
-    //Consider @ts-expect-error
-    | Getter<typeof RSome>
-    | Getter<typeof RProps>
-    
-    expect(true).toBe(true)
-  })
-
-  it("Function", () => {
-    function RWithout(_: Without) {return null};
-    function RWrong(_: Wrong) {return null};
-    function RSome(_: Some) {return null};
-    function RProps(_: Props) {return null};
-
-    // type RF = (props: {classnames: any}) => ReactElement<any, any> | null
-
-    // const ch1: Props extends {classnames: any} ? true : false = true
-    // const ch2: Parameters<typeof RProps>[0] extends {classnames: any} ? true : false = true
-    // const ch3: Parameters<typeof RProps>[0] extends Parameters<RF>[0] ? true : false = true
-
-    //@ts-expect-error
-    type Res =
-    //TODO @ts-expect-error
-    | Getter<typeof RWithout>
-    //TODO @ts-expect-error
-    | Getter<typeof RWrong>
-    //Consider @ts-expect-error 
-    | Getter<typeof RSome>
-    | Getter<typeof RProps>
-    
-    expect(true).toBe(true)
-  })
-
-
 })
