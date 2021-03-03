2f966ee8bdb1649a3fa9bc820426ccb2ea611e66
Andrii Kirmas <kirmas@phystech.edu>
2021-03-03 21:11:42 +0300

    #11 Change test pipe to vars for easier development

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,23 +1,30 @@
 import classNaming from "."
 
-
 describe("#11 className control", () => {
   it("no className", () => {
     const classes = classNaming()
+    //TODO #11 @ts-expect-error
+    , call1 = classes(true)
+    , call2 = classes()
+    //TODO #11 @ts-expect-error
+    , call3 = call2(true)
 
-    expect({...classes(
-      //TODO #11 @ts-expect-error
-      true
-    )}).toStrictEqual({
-      className: ""
-    })
+    expect([
+      {...call1},
+      {...call3}
+    ]).toStrictEqual([
+      {className: ""},
+      {className: ""}
+    ])
   })
   it("className double use", () => {
     const classes = classNaming({className: "class", classnames: {}})
-    expect({...classes(true)(
-      //TODO #11 @ts-expect-error
-      true
-    )}).toStrictEqual({
+    , call1 = classes()
+    , call2 = call1(true)
+    //TODO #11 @ts-expect-error
+    , call3 = call2(true)
+
+    expect({...call3}).toStrictEqual({
       className: "class class"
     })
   })
