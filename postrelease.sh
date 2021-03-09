21123e5716b95ea2ca64c892156ad66ae3312338
Andrii Kirmas <kirmas@phystech.edu>
2021-03-09 16:04:43 +0300

    Fix postrelease.sh to not be squashed but with merge-commit

--- postrelease.sh
+++ postrelease.sh
@@ -2,7 +2,7 @@
 v="$npm_package_version"
 
 git switch main
-git merge --squash -X theirs release -nm "Backmerge after v$v release"
+git merge -X theirs -nm "Backmerge after v$v release" release
 npm update; npm update --save-dev
 git commit -m "Updater after v$v release"
 exit $?
