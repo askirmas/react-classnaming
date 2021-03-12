add6e1cf2e658cf8a721bbc26d42d19538086a5e
Andrii Kirmas <kirmas@phystech.edu>
2021-03-12 19:48:37 +0300

    Change tagging branch to be `main` back

--- postrelease.sh
+++ postrelease.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 v="$npm_package_version"
 
-git switch main
+# git switch main
 # Change strategy to not mesh history
-git merge -X theirs -nm "Backmerge after v$v release" release
+# git merge -X theirs -nm "Backmerge after v$v release" release
 npm update; npm update --save-dev
 git add .
 git commit -m "Updater after v$v release"
